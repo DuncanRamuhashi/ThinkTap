4d7ab070fef775e4779a20a0841e690eb24aafe4
Duncan Ramuhashi <duncanramohashi@gmail.com>
2026-05-28 19:00:53 +0200

    set

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import { Env_Consts } from './constants/envConsts';
-import connectDB from './config/db.js';
-import { httpCodes } from "../constants/envConsts";
+import express from "express";
+import cors from "cors";
+
+import { Env_Consts } from "./constants/envConsts.js";
+import connectDB from "./config/db.js";
+
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
+
 // Middleware
-
-
 app.use(cors({
   origin: [
     Env_Consts.FRONTEND_URL || "http://localhost:5000",
   ],
   credentials: true,
 }));
-app.use(cors());
-app.use(express.json());
-app.use(express.json({ limit: '5mb' })); // just for files if i need to
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get("/", (req, res) => {
-  res.status(httpCodes.OK).json({
+  res.status(200).json({
     message: "Welcome to the API",
   });
 });
