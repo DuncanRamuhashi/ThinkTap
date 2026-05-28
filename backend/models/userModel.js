539c996afd49fb0db0a6872954b72fb8f3193e6d
Duncan Ramuhashi <duncanramohashi@gmail.com>
2026-05-28 19:49:20 +0200

    user set

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+    type: String,
+    enum: ["captain", "thinker"],
+    default: "thinker",
+    },
   },
   {
     timestamps: true,
